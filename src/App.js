34166c826ec9deab92ad99e2fe62f224a2666c4d
teffcode <tefy.1@live.com>
2018-09-24 15:44:27 -0500

    🌸 Initial data & buttons prev/next

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+import data from './data/data';
+
+import './App.css';
+
+class App extends Component {
+  state = {
+      properties: data.properties, 
+      property: data.properties[0] 
+  }
+
+  prevProperty = () => {
+    const newIndex = this.state.property.index-1;
+    this.setState({
+      property: data.properties[newIndex]
+    })
+  }
+
+  nextProperty = () => {
+    const newIndex = this.state.property.index+1;
+    this.setState({
+      property: data.properties[newIndex]
+    })
+  }
+
+  render() {
+    const { properties, property } = this.state;
+    console.log(data.properties.length)
+
+    return (
+      <div>
+        <button 
+          onClick={() => this.prevProperty()}
+          disabled={property.index === 0}
+        > PREV </button>
+        <button 
+          onClick={() => this.nextProperty()}
+          disabled={property.index === data.properties.length-1}
+        > NEXT </button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/data/data.js
@@ -0,0 +1,18 @@
+const data = {
+    "properties": [
+        {
+            "index": 0,
+            "name": "Tefa"
+        },
+        {
+            "index": 1,
+            "name": "Tity"
+        },
+        {
+            "index": 2,
+            "name": "Santi"
+        }
+    ]
+}
+
+export default data;
